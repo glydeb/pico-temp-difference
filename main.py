8fd60a3d1ccb395cc3d8b97f01f598ff0e82711c
Glydewell Burdick <glydewellb@gmail.com>
2024-09-30 14:22:54 -0500

    move callback to main function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time, machine
 from wlan import connect_wifi
 from flash import flash_led
-from message_callback import on_message
 import read_sensors
 import umqtt.simple
 import mqtt
+
+
 
 def main():
     # Connect to WiFi
@@ -24,20 +25,24 @@ def main():
         time.sleep(60)
         print(e)
 
-        def on_message(topic, msg):
-            nonlocal hvac_mode
-            if topic == b"climate/system_mode":
-                hvac_mode = msg.decode()
-
     # Listen for a message on topic climate/system_mode
+    hvac_mode = b'idle'
+
+    def on_message(topic, message):
+        # Perform desired actions based on the subscribed topic and response
+        print('Received message on topic:', topic)
+        print('Response:', message)
+        nonlocal hvac_mode
+        hvac_mode = message.decode()
+
     mqtt_client.set_callback(on_message)
     mqtt_client.subscribe(b"climate/system_mode")
-    hvac_mode = b'idle'
 
     while True:
         mqtt_client.check_msg()
+        print(hvac_mode)
         # when system is on, read sensors and publish delta_t
-        if hvac_mode in [b'heating', b'cooling']:
+        if hvac_mode in ['heating', 'cooling']:
             sensor_data = read_sensors.get_SAT_RAT()
             if sensor_data is None or sensor_data['SAT'] is None or sensor_data['RAT'] is None:
                 continue
@@ -45,7 +50,7 @@ def main():
             delta_t = (sensor_data['SAT'] - sensor_data['RAT']) * mode_adjustment
             print(sensor_data)
             mqtt_client.publish(b"pico/delta_t", str(delta_t).encode())
-        time.sleep(1)  # Add a small delay to avoid busy-waiting
+        time.sleep(10)  # Add a small delay to avoid busy-waiting
 
 if __name__ == "__main__":
     main()
